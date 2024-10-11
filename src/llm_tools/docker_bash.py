1ad569215f8d5745966ef72e1eba1771e453cf27
Andrew Wason <rectalogic@rectalogic.com>
2024-10-11 15:09:08 -0400

    simplify docker

--- src/llm_tools/docker_bash.py
+++ src/llm_tools/docker_bash.py
@@ -4,28 +4,26 @@
 import json
 import shutil
 import subprocess
-from typing import Annotated
+from typing import Annotated, Optional
 
 
 class DockerBash:
     """Execute a `bash` shell command on the local Linux system."""
 
     __name__ = "bash"
+    container: Optional[str] = None
 
     def __init__(self):
         self.docker = shutil.which("docker")
         if not self.docker:
-            raise ValueError("docker is not installed")
-        self.process = subprocess.Popen(  # noqa: S603
-            [self.docker, "run", "--rm", "-i", "ubuntu:latest", "bash"],
+            raise ValueError("Unable to find docker command")
+        completed = subprocess.run(  # noqa: S603
+            [self.docker, "run", "--rm", "--detach", "ubuntu:latest", "/usr/bin/sleep", "infinity"],
+            capture_output=True,
             text=True,
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
+            check=True,
         )
-        self.process.stdin.write("hostname\n")
-        self.process.stdin.flush()
-        self.container = self.process.stdout.readline().strip()
+        self.container = completed.stdout.strip()
 
     def __call__(self, command_line: Annotated[str, "A `bash` shell command line to execute."]) -> str:
         completed = subprocess.run(  # noqa: S603
@@ -34,12 +32,11 @@ class DockerBash:
             text=True,
         )
 
-        print("result", completed.stdout, completed.stderr)
         result = {"stdout": completed.stdout, "stderr": completed.stderr}
         if completed.returncode != 0:
             result["is_error"] = True
         return json.dumps(result)
 
     def __del__(self):
-        if self.process:
-            self.process.terminate()
+        if self.container:
+            subprocess.run([self.docker, "stop", self.container])
